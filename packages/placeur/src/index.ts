e7c7dc0b18b2c55d0ad61f459002330d23064f9e
Moritz Vifian <moritz.vifian@gmx.ch>
2026-05-31 20:55:47 +0200

    clean up

--- packages/placeur/src/index.ts
+++ packages/placeur/src/index.ts
@@ -1,5 +1,3 @@
-export { placeur } from './placeur.js'
-export { flowLayout } from './pack-flow.js'
 export type {
   Bin, Block, ColumnLayout, FlowLayoutOptions, PlacedBin, PlacedBlock, PlaceurOptions, PlaceurResult, FlowLayoutResult,
 } from './api-types.js'
